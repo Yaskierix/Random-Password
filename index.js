12ce2cc216413f278f7e4415d0a1f7fd590bf8d0
Yaskierix <bokserpol1@gmail.com>
2025-07-23 13:38:30 +0200

    CSS to SCSS

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import './style.scss';
+
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 let randomIndex = null;
@@ -13,6 +15,8 @@ function generatePassword(){
   output1.textContent = randomPassword.join("");
   randomPassword = [];
 }
+
+window.generatePassword = generatePassword;
 
 
 
